997047e9dd8f16e20a5ecfe673f1da8b3b2a1153
Sam Wray <sam@wray.pro>
2022-01-12 21:53:59 +0000

    fix(jsconf-mexico): updates all timezones to use UTC

--- docs/.vuepress/store/modules/events.js
+++ docs/.vuepress/store/modules/events.js
@@ -4,28 +4,29 @@ const state = {
       title: "Modular Audio Visualisation with modV @ JSConf Mexico",
       location: "Online",
       url: "https://jsconf.mx/en",
-      date_start: new Date("12/01/2021 21:00 CET"),
-      date_end: new Date("12/01/2021 22:30 CET")
+      date_start: new Date("12/01/2021 21:00 UTC+1"),
+      date_end: new Date("12/01/2021 22:30 UTC+1")
     },
-    
+
     {
       title: "CODAME ART+TECH Festival",
       location: "Online",
       url: "https://codame.com/events/modular-audio-visualisation-with-modv",
-      date_start: new Date("12/16/2020 11:30 PST"),
-      date_end: new Date("12/16/2020 14:00 PST")
+      date_start: new Date("12/16/2020 11:30 UTC-8"),
+      date_end: new Date("12/16/2020 14:00 UTC-8")
     },
 
     {
       title: "Visual & Light Workshop @ Dunarnia",
       location: "Szentendre, Hungary",
-      url: "https://www.evensi.com/visual-light-workshop-dunarnia-eter-a-6/330334118",
+      url:
+        "https://www.evensi.com/visual-light-workshop-dunarnia-eter-a-6/330334118",
       date_start: new Date("08/31/2019 08:00 UTC+1"),
       date_end: new Date("08/31/2019 14:00 UTC+1")
     }
   ]
-}
+};
 
 export default {
   state
-}
+};
