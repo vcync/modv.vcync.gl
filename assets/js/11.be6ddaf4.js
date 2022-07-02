63e6aab8e1bc5a2e4e430710d597f5826d20dc3e
2xAA <sam@wray.pro>
2022-07-02 22:56:45 +0000

    Deploying to gh-pages from @ vcync/modv.vcync.gl@a6188b5e79e4e26f8d35f5d9644abfbcb97dea10 🚀

---
+++ assets/js/11.be6ddaf4.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[11],{233:function(n,t,e){"use strict";e.r(t);var s=e(6),o=Object(s.a)({},(function(){var n=this,t=n._self._c;return t("ul",n._l(n.$store.getters.previous_workshops,(function(e,s){return t("li",{key:s},[n._v("\n      "+n._s(e.title)+" - "+n._s(e.location)+".\n    ")])})),0)}),[],!1,null,null,null);t.default=o.exports}}]);
